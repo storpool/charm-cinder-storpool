41588b477293500cb4adebe8ac7c45887fe63f6e
Peter Pentchev <pp@storpool.com>
2017-08-14 11:02:19 -0400

    More robust local configuration.

--- reactive/storpool-cinder-charm.py
+++ reactive/storpool-cinder-charm.py
@@ -14,11 +14,6 @@ def rdebug(s):
 	with open('/tmp/storpool-charms.log', 'a') as f:
 		print('{tm} [cinder-charm] {s}'.format(tm=time.ctime(), s=s), file=f)
 
-@reactive.hook('config-changed')
-def whee():
-	rdebug('wheeeeeee config-changed')
-	hookenv.status_set('maintenance', 'waiting for stuff to happen') # or just ''
-
 @reactive.when('storpool-config.available')
 def get_storpool_config(hconfig):
 	rdebug('apparently the storpool-config hook is available now: {h}'.format(h=hconfig))
@@ -29,39 +24,44 @@ def get_storpool_config(hconfig):
 			rdebug('no config yet...')
 			reactive.remove_state('storpool-cinder.configure')
 			reactive.remove_state('storpool-cinder.configured')
+			return
+		rdebug('  - we have *some* configuration, checking for our node {node}'.format(node=sp_node))
+		nodeconf = conf.get(sp_node, None)
+		if nodeconf is None:
+			rdebug('no config from our node yet...')
+			reactive.remove_state('storpool-cinder.configure')
+			reactive.remove_state('storpool-cinder.configured')
+			return
+		rdebug('  - yeah, we are on!')
+		if helpers.data_changed('storpool-cinder.conf', nodeconf) or not helpers.is_state('storpool-cinder.configured'):
+			rdebug('  - and something changed, what do you know?')
+			reactive.set_state('storpool-cinder.configure')
+			reactive.remove_state('storpool-cinder.configured')
 		else:
-			rdebug('  - yeah, baby, we are on!')
-			if helpers.data_changed('storpool-cinder.conf', conf):
-				rdebug('  - and something changed, what do you know?')
-				reactive.set_state('storpool-cinder.configure')
-				reactive.remove_state('storpool-cinder.configured')
+			rdebug('  - but nothing changed for our node {node}'.format(node=sp_node))
 	except Exception as e:
 		rdebug('could not examine the hook conversation: {e}'.format(e=e))
 
 @reactive.when_not('storpool-config.available')
-@reactive.when_not('storpool-cinder.whee')
+@reactive.when_not('storpool-cinder.configured')
 def no_whee_yet():
 	rdebug('hm, we do not have a storpool-config hook yet?')
-	reactive.remove_state('storpool-cinder.configure')
-	reactive.remove_state('storpool-cinder.configured')
 	hookenv.status_set('maintenance', 'waiting for the storpool-config hook')
 
-@reactive.when_not('storpool-config.available')
-@reactive.when('storpool-cinder.whee')
-def no_more_config():
-	rdebug('erm, we lost the config, did we not?')
-	reactive.remove_state('storpool-cinder.whee')
-	reactive.remove_state('storpool-cinder.configure')
-	reactive.remove_state('storpool-cinder.configured')
-	hookenv.status_set('maintenance', 'waiting for the storpool-config hook to be reestablished')
-
 @reactive.when('storpool-cinder.configure')
+@reactive.when_not('storpool-cinder.configure')
 @reactive.when('storpool-config.available')
 def configure(hconfig):
 	rdebug('trying to configure the StorPool backend of the Cinder installation')
 	reactive.remove_state('storpool-cinder.configure')
 
-	rdebug('now let us pretend we actually did something with the {hconfig} hook, okay?'.format(hconfig=hconfig))
+	conf = hconfig.get_storpool_config()
+	if conf is None:
+		rdebug('erm, how did we get here with no configuration at all?')
+	nodeconf = conf.get(sp_node, None)
+	if nodeconf is None:
+		rdebug('erm, how did we get here with no node configuration?')
+	rdebug('now let us pretend we actually did something with the data')
 
 	reactive.set_state('storpool-cinder.configured')
 	hookenv.status_set('active', 'up and running and configured')
